e1b641a8a09c7746ef7fbefdf6c753dbeefbc3be
Thomas Yang <thomasyang127@yahoo.com>
2018-10-10 20:39:44 -0500

    Added Writeup; current working commit.

--- ID3.py
+++ ID3.py
@@ -1,4 +1,5 @@
 from node import Node
+from collections import Counter
 import math
 
 def ID3(examples, default):
@@ -7,7 +8,44 @@ def ID3(examples, default):
   trained on the examples.  Each example is a dictionary of attribute:value pairs,
   and the target class variable is a special attribute with the name "Class".
   Any missing attributes are denoted with a value of "?"
+
+  returns tree root (Node)
   '''
+
+  out = Node()
+  out.label = default
+  if len(examples) == 0: 
+    return out
+  
+  #Count of how many class types there are in total
+  classification = [ex['Class'] for ex in examples]
+  targetCount = Counter(classification)
+
+  if len(examples[0]) <= 1:
+    out.label = max(classification, key=targetCount.get)
+    return out
+
+  if len(targetCount) <= 1:
+    out.label = targetCount.elements()[0]
+    return out
+
+  # calculate initial entropy
+
+  # for each (remaining attribute), partition based on the attribute; 
+
+  # find the attribute that results in the greatest reduction in entropy
+
+  # change the label to None; change the attribute to the best attribute
+
+  # Run ID3 on the partitioned examples, remembering to REMOVE from each, the attribute that has been split. For each new
+  # tree created, remember to add the children based on the label of the examples. 
+
+  #return the created node
+
+  
+  
+
+
 
 def prune(node, examples):
   '''
@@ -15,10 +53,14 @@ def prune(node, examples):
   to improve accuracy on the validation data; the precise pruning strategy is up to you.
   '''
 
+  # Initial strategy: Reduced error pruning
+
+  
+
 def test(node, examples):
   '''
-  Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
-  of examples the tree classifies correctly).
+  Takes in a trained tree and a test set of examples. Returns the accuracy (fraction
+  of examples the tree classifies correctly). 
   '''
 
 
--- node.py
+++ node.py
@@ -1,5 +1,11 @@
 class Node:
   def __init__(self):
-    self.label = None
+    self.label = None #None for non-leaf branches; otherwise, label with the actual label
     self.children = {}
+    self.attribute = None #None for leaf; attribute for actual; Attribute is a string. 
+
+    self.misclassified = 0 #only used for testing... 
+
+    #include information for pruning; if we find a contradictory test set; 
+
 	# you may want to add additional fields here...
